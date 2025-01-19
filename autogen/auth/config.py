a28f4c515b70c104bc98a28f035b1ce2b30e0f0b
sanket <sanketmaheshwari96@gmail.com>
2025-01-19 17:22:27 +0530

    removed static path from config

--- autogen/auth/config.py
+++ autogen/auth/config.py
@@ -2,7 +2,7 @@ import os
 import json
 import sys
 
-with open("/Users/sanket/Personal/LLM/LangchainTutorial/config.json") as f:
+with open("config.json") as f:
     env_vars = json.load(f)
 
 HUGGINGFACEHUB_API_TOKEN = env_vars["HF_API_KEY"]
